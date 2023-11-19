139a62b07749caf47a6a7340beb3a16c7e1e0f7d
lcastienc <2023_luis.castillo@iticbcn.cat>
2023-11-19 23:55:36 +0100

    Parte1

--- JocDelPenjat/index.js
+++ JocDelPenjat/index.js
@@ -1,3 +1,7 @@
+let juegosIniciados = 0;
+let juegosGanados = 0;
+let juegosPerdidos = 0;
+
 //Mostrar las opciones por consola
 function menuGame() {
     console.log("Menú del juego:");
@@ -10,7 +14,7 @@ function menuGame() {
 function opcionJugador() {
     let opcionEscogida;
 
-    while (true) {
+    do {
         menuGame();
         opcionEscogida = parseInt(prompt("¿Qué quieres hacer?"));
 
@@ -25,11 +29,62 @@ function opcionJugador() {
                 break;
             case 3:
                 console.log("Has salido del juego");
-                return;
+                return; // Salir del programa
             default:
                 console.log("Opción no válida. Por favor, elige de nuevo.");
         }
+    } while (true);
+}
+opcionJugador();
+
+function iniciarJoc() {
+    let palabraIntroducida = prompt("Introduce la palabra a adivinar: ");
+    let palabraGuionada = "_".repeat(palabraIntroducida.length);
+    const maxIntentos = 6;
+    let intentosRealizados = 0;
+    let letrasMalas = [];
+
+    console.log(`La palabra a adivinar es: ${palabraGuionada.split('').join(' ')}`);
+
+    while (palabraGuionada.includes("_") && intentosRealizados < 6) {
+
+        let posibleLetra = prompt("Introduce una letra: ");
+
+        if (posibleLetra.length !== 1 || !(/[a-zA-Z]/).test(posibleLetra)) {
+            console.log("Has introducido más de una letra o un numero");
+            continue;
+        }
+
+        if (palabraIntroducida.includes(posibleLetra)) {
+            for (let index = 0; index < palabraIntroducida.length; index++) {
+                if (palabraIntroducida[index] === posibleLetra) {
+                    palabraGuionada = palabraGuionada.substring(0, index) + posibleLetra + palabraGuionada.substring(index + 1);
+                }
+            }
+        } else {
+            intentosRealizados++;
+            letrasMalas.push(posibleLetra);
+        }
+
+        console.log(`Palabra invisible: ${palabraGuionada.split('').join(' ')}`);
+        console.log(`Letras fallidas ${intentosRealizados}/${maxIntentos}: ${letrasMalas.join(', ')}`);
     }
+
+    if (!palabraGuionada.includes("_")) {
+        console.log("Has ganado, has adivinado la palabra");
+        juegosGanados++;
+    } else {
+        console.log(`Has perdido, la palabra a adivinar era: ${palabraIntroducida}`);
+        juegosPerdidos++;
+    }
+    juegosIniciados++;
+    opcionJugador();
 }
 
-opcionJugador();
+function mostrarEstadisticas() {
+    console.log(`Total de partidas jugadas: ${juegosIniciados}`);
+    console.log(`Partidas ganadas (${((juegosGanados / juegosIniciados) * 100).toFixed(2)}%): ${juegosGanados}`);
+    console.log(`Partidas perdidas (${((juegosPerdidos / juegosIniciados) * 100).toFixed(2)}%): ${juegosPerdidos}`);
+   
+    opcionJugador();
+}
